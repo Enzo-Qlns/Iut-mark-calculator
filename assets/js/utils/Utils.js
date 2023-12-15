0fb9ac36037904cc421d4c791a1b5c28318b759c
Enzo-Qlns <enzoquelenis@gmail.com>
2023-12-15 16:55:36 +0100

    refacto

--- assets/js/utils/Utils.js
+++ assets/js/utils/Utils.js
@@ -1,4 +1,17 @@
 const Utils = {
+    isEmpty: function(...values) {
+        if (values.length === 0)
+            return true;
+        for (let index = 0; index < values.length; index++) {
+            const value = values[index];
+            const isArray = Array.isArray(value);
+            let bool = (undefined === value || null === value || (isArray && value.length === 0) || (typeof value === 'string' && value.length === 0));
+            if (bool)
+                return true;
+        }
+        return false;
+    },
+    
     sumArray: function (arr) {
         return arr.reduce((a, b) => a + b, 0);
     },
@@ -19,19 +32,8 @@ const Utils = {
     deleteArrayValue: function (arr, value) {
         return arr.filter(x => x !== value);
     },
-    isEmpty(...values) {
-        if (values.length === 0)
-            return true;
-        for (let index = 0; index < values.length; index++) {
-            const value = values[index];
-            const isArray = Array.isArray(value);
-            let bool = (undefined === value || null === value || (isArray && value.length === 0) || (typeof value === 'string' && value.length === 0));
-            if (bool)
-                return true;
-        }
-        return false;
-    },
-    boolToValue(bool) {
+
+    boolToValue: function(bool) {
         return bool ? 'OUI' : 'NON';
     }
 }
