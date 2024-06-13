54a17032725eb45cd0e7c93ec29d15a29de4b21a
Enzo QUELENIS <equelenis@myges.fr>
2024-06-13 20:27:41 +0200

    add paypal popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,30 @@ const styleElement = document.createElement('link');
 styleElement.href = 'https://cdn.jsdelivr.net/npm/apexcharts@3.40.0/dist/apexcharts.min.css';
 document.head.appendChild(styleElement);
 
+Date.isLeapYear = function (year) {
+    return (((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0));
+};
+
+Date.getDaysInMonth = function (year, month) {
+    return [31, (Date.isLeapYear(year) ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
+};
+
+Date.prototype.isLeapYear = function () {
+    return Date.isLeapYear(this.getFullYear());
+};
+
+Date.prototype.getDaysInMonth = function () {
+    return Date.getDaysInMonth(this.getFullYear(), this.getMonth());
+};
+
+Date.prototype.addMonths = function (value) {
+    let n = this.getDate();
+    this.setDate(1);
+    this.setMonth(this.getMonth() + value);
+    this.setDate(Math.min(n, this.getDaysInMonth()));
+    return this;
+};
+
 // Action after click
 let isClicked = false;
 const buttonMark = createButton("Afficher vos moyennes");
@@ -12,8 +36,17 @@ buttonMark.addEventListener('click', (event) => {
     const title = document.querySelector("#mainContent > div > div:nth-child(6) > div > h4");
 
     if (!Utils.isEmpty(title, averageDataByUE) && title.textContent === "Modalités de Contrôle des Connaissances") {
-        // toastPaypal(() => generateHtml(averageDataByUE));
-        generateHtml(averageDataByUE);
+        let date = new Date();
+        let validate_don = !Utils.isEmpty(localStorage.getItem('validate_don')) ? new Date(localStorage.getItem('validate_don')) : null;
+        if (Utils.isEmpty(validate_don)) {
+            toastPaypal(() => generateHtml(averageDataByUE));
+            localStorage.setItem('validate_don', date.toString());
+        } else if (validate_don > new Date().addMonths(1)) {
+            toastPaypal(() => generateHtml(averageDataByUE));
+            localStorage.setItem('validate_don', date.toString());
+        } else {
+            generateHtml(averageDataByUE);
+        }
     }
 
     if (Utils.isEmpty(Object.values(averageDataByUE))) {
@@ -21,5 +54,5 @@ buttonMark.addEventListener('click', (event) => {
     } else if (isClicked) {
         event.target.classList.add('disabled');
         buttonMark.querySelector('i').classList.replace("fa-eye", "fa-eye-slash");
-    };
+    }
 });
